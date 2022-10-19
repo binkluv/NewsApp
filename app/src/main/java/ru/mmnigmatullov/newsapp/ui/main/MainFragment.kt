285162f7eb458e3ed77fd5d112ae466a021eae42
binkluv <marsel5657@gmail.com>
2022-10-19 15:02:18 +0300

    Lesson-2

---
+++ app/src/main/java/ru/mmnigmatullov/newsapp/ui/main/MainFragment.kt
@@ -0,0 +1,21 @@
+package ru.mmnigmatullov.newsapp.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.mmnigmatullov.newsapp.R
+
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
